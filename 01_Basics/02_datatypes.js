f31c75c21877fbe42a0c0e5232098b136e934347
Himanshi Negi <91870160+hnegi2504@users.noreply.github.com>
2023-09-30 20:51:12 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict";
+
+//alert("hello")
+
+let name = "hi" //string
+let age = 18 //number
+let islog = false //boolean
+let state = null
+
+// Datatypes: number , string, bigint, symbol, null, undefined, object,boolean
+
+console.log(typeof("hi"));
+console.log(typeof boolean);
