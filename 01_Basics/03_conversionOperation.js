eefa56496033ff0682b586a95a17e128c4831627
Himanshi Negi <91870160+hnegi2504@users.noreply.github.com>
2023-09-30 21:18:41 +0000

    conversionOperation done

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+// let score = 33
+// console.log (typeof(score));
+// let value = String(score);
+// console.log (typeof(value));
+
+// *********************************************************
+
+// let score = "33abc"
+// console.log (typeof(score));
+// let value = Number(score);
+// console.log (typeof(value));
+// console.log(value);
+
+
+// ********************************************************
+
+
+// let score = 0
+// console.log (typeof(score));
+// let value = Boolean(score);
+// console.log (typeof(value));
+// console.log(value);
+
+// // *********************OPERATIONS***********************************
+
